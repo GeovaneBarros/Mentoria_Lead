7e24fd9b2726170fd0ffba129745dcebaa8c0837
Geovane Barros <geovanebarros@pop-os.localdomain>
2021-10-29 01:33:08 -0300

    Finalização ativiade 5 e 6

--- exercicios6.py
+++ exercicios6.py
@@ -1,7 +1,13 @@
 import regex as re
 
+frase = '(85)985558599'
+if re.search('[-|+]?\d+([.]\d+)?',frase)[0] == frase:
+    print('Valid')
+else:
+    print('Not valid')
 
+if re.search("[(?]\d?\d\d[)?] ?\d\d\d\d\d[ |-]?\d\d\d\d", frase)[0] == frase:
+    print('Celular')
+else:
+    print('Fixo')
 
-
-frase = 'Uma borboleta 3 '
-print(re.search('-?\d',frase))
